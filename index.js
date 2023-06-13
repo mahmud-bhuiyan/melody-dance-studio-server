5860c9808fe3215397a49552c26405663577da12
mahmud-bhuiyan <bh.mahmud@gmail.com>
2023-06-14 04:21:08 +0600

    user role updated to admin

--- index.js
+++ index.js
@@ -98,6 +98,19 @@ async function run() {
       res.send(result);
     });
 
+    //update user role to admin
+    app.patch("/users/admin/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          role: "admin",
+        },
+      };
+      const result = await usersCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    });
+
     // ------------------------------
     // ------------------------------
     await client.db("admin").command({ ping: 1 });
