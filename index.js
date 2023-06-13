f2cda0ab432b309a771f248ad8fd9eabdd615621
mahmud-bhuiyan <bh.mahmud@gmail.com>
2023-06-13 19:30:03 +0600

    mongo db connected

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+require("dotenv").config();
 const port = process.env.PORT || 7000;
 
 //middleware
 app.use(cors());
 app.use(express.json());
+
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.5nrgbhc.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } catch {
+    console.log("An error occurred");
+  }
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("melody-dance-studio-server is running");
